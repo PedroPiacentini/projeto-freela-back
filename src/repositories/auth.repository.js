1b6f68c7c25bbf771ae111a3ec1ce6066481c2f9
Pedro <pedropiacentini@usp.br>
2023-05-29 09:45:15 -0300

    fix: query

--- src/repositories/auth.repository.js
+++ src/repositories/auth.repository.js
@@ -1,12 +1,12 @@
 import { db } from "../database/db.connection.js";
 
 export function getUserByEmailDB(email) {
-    return db.query(`SELECT * FROM users WHERE email=$1;`, [email]);
+    return db.query(`SELECT * FROM usuarios WHERE email=$1;`, [email]);
 }
 
 export function createUserDB(name, email, password) {
     return db.query(
-        `INSERT INTO user (name, email, senha) VALUES ($1s, $2, $3)`,
+        `INSERT INTO usuarios (nome, email, senha) VALUES ($1s, $2, $3)`,
         [name, email, password]
     );
 }
